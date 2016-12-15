c1e8b1e9f971a9eee57210c78217009dbea5ae79
Matthew Kohut <mkohut95@gmail.com>
2016-12-15 15:58:18 -0500

    Working project!

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 
 $(document).ready(function() {
 
-	function addDivs() {
-	for (var i = 1; i <= 256; i++) {
-		$('#wrapper').append('<div class="grid" id="div'+i+'"></div>');
-	}
-	}
-	addDivs();
-
-	$('.grid').hover(function() {
-		$(this).addClass('hover');
-	});
+	makeGrid(16*16);
+	makeNewGrid();
 
 });
+
+
+
+function makeGrid(size) {
+
+	for (var i = 1; i <= size; i++) {
+		$('#wrapper').append('<div class="grid"></div>');
+	}
+
+	var newSize = $('#wrapper').height() / Math.sqrt(size);
+	$('.grid').height(newSize);
+	$('.grid').width(newSize);
+
+	color();
+}
+
+
+function makeNewGrid() {
+	$('button').click(function() {
+    input = prompt("Pick a grid size! (e.g. enter 16 for 16x16 grid)");
+
+    $('#wrapper').empty();
+	makeGrid(Math.pow(input, 2));
+	});
+
+	color();
+};
+
+function color() {
+	$('.grid').mouseenter(function() {
+		$(this).addClass('hover');
+	});
+}
